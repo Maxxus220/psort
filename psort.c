2c27f55d2102a62bcdcb693ff6b72f5fcef5e088
Maxxus220 <Maxxus220@gmail.com>
2022-11-04 18:04:57 -0500

    Pretty well outlined

--- psort.c
+++ psort.c
@@ -2,8 +2,70 @@
 #include <stdio.h>
 
 
-int main() {
+int main(int argc, char** argv) {
+    const int OVERSAMPLING_FACTOR = 3;
+    const int NUM_CORES;
+    const int ENTRY_DATA_SIZE = 96;
 
-    printf("Hello World");
+    // Check if valid input and get filenames from args
+
+    // Map input file
+
+    // Sample sort file
+
+    // Map output file
+
+    // Copy sorted records over
+
+    // Clean up both maps
+    
+    return 0;
+}
+
+int mapFile(void** map, int* fd, char* fileName, int prot) {
+    // Open file
+
+    // Get fd
+
+    // Get file size
+
+    // Map file
+
+    // Set fd
+
+    return 0;
+}
+
+int cleanUpMap(void* map, int fd) {
+    // Munmap
+
+    // Close file
+
+    return 0;
+}
+
+int sampleSort(void* arr, int k, int p, int size) {
+
+    // Select (p-1)*k samples
+
+    // Select p bucket bounds
+
+    // Place arr entries into p buckets
+
+    // Start quicksort for each thread on a bucket
+
+    return 0;
+}
+
+int quickSort(void* arr, int size) {
+
+    // Select first element as pivot
+
+    // Partition other elements
+
+    // Call quick sort on partitions
+
+    // Combine
+
     return 0;
 }
